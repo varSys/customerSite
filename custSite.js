24387c207aa25e8b6678663ce53ea77ae91b522b
shotofd <davidmaharry@gmail.com>
2015-05-31 17:54:20 -0400

    Cleaned up structure again. Moved reveal.js to bower_components folder. Got rid of node stuff. Added footer.

--- custSite.js
+++ custSite.js
@@ -4,3 +4,29 @@ var customerSite = angular.module('customerSite', []);
 customerSite.controller('header', function ($scope) {  
     $scope.boxes = []
 });
+customerSite.controller('footer', function ($scope) {  
+    $scope.boxes = []
+});
+
+//on page load
+$(document).ready(function() {
+	 $(".footer").hide();
+	 
+	 if($(window).height() == $(document).height()){
+	 $(".footer").show();
+   }
+});
+
+// on scroll functions
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+	   $(".footer").show();
+   }
+});
+
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() != $(document).height()) {
+	 $(".footer").hide();
+   }
+});
+	
